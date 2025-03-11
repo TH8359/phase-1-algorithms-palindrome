43459007d235be702af58487f0c03a5cd0e326bf
Tyler Hogue <hogue73@embarqmail.com>
2025-03-11 16:27:53 -0400

    done

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  //Forms the reversed word
+  let reversedWord = ""
+  let currentArrayPosition = word.length - 1
+  while (currentArrayPosition >= 0){
+    let currentLetter = word[currentArrayPosition]
+    reversedWord = reversedWord + currentLetter
+    currentArrayPosition--
+  }
+
+  if (reversedWord === word){
+    return true
+  }else{
+    return false
+  }
 }
 
 /* 
@@ -8,6 +23,11 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+
+  //First, a while loop is used to create a reversed version of the provided word,
+  which is assigned as a value to the reversedWord variable. Once all of that is done,
+  the values of word and reversedWord are compared. If they are the same, the code
+  returns true. If not, the code returns false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +36,11 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("noon"))
+
+  console.log("Expecting: false")
+  console.log("=>", isPalindrome("orange"))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
